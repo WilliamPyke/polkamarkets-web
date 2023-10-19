88e30cf2ae0b484dff25d2a8070b99da45f180bc
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-10-19 16:13:22 +0100

    feat(MarketFooterActions): customizable twitter share

--- src/components/Market/MarketFooterActions.tsx
+++ src/components/Market/MarketFooterActions.tsx
@@ -4,6 +4,8 @@ import { Market } from 'models/market';
 import FavoriteMarket from 'components/FavoriteMarket';
 import Icon from 'components/Icon';
 
+import { environment } from 'config';
+
 type MarketFooterActionsProps = {
   market: Market;
   $variant: 'filled' | 'text';
@@ -13,10 +15,18 @@ export default function MarketFooterActions({
   market,
   $variant = 'text'
 }: MarketFooterActionsProps) {
+
+  // stripping protocol from publicUrl
+  const publicUrl = environment.PUBLIC_URL?.replace(/(^\w+:|^)\/\//, '') || '';
+
+  const twitterShareIntent =
+    environment.UI_TWITTER_SHARE_INTENT ||
+    'I%20just%20made%20a%20prediction%20on%20@polkamarkets!%20🔥💯%0D%0D';
+
   return (
     <div className="pm-c-market-footer__actions">
       <a
-        href={`https://twitter.com/intent/tweet?text=I%20just%20made%20a%20prediction%20on%20@polkamarkets!%20🔥💯%0D%0DCheck%20it%20out%20at%20app.polkamarkets.com/markets/${market.slug}%20%23polkamarkets`}
+        href={`https://twitter.com/intent/tweet?text=${twitterShareIntent}${publicUrl}/markets/${market.slug}`}
         target="_blank"
         rel="noreferrer"
         className={cn('pm-c-market-footer__actions-button', {
