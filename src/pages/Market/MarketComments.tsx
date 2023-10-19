cfc1a520263c33406cf3d151fcbf3d0bf5316925
Wellington Matheus <w.matheus@outlook.com>
2023-10-19 16:36:44 -0300

    feat(MarketComments): add `ProfileSignin`

--- src/pages/Market/MarketComments.tsx
+++ src/pages/Market/MarketComments.tsx
@@ -4,19 +4,32 @@ import isNull from 'lodash/isNull';
 import type { Comment } from 'types/market';
 import Avatar from 'ui/Avatar';
 
-import { Button } from 'components';
+import { Button, Icon, ProfileSignin } from 'components';
 
 import { useAppSelector, useLanguage } from 'hooks';
 
 import styles from './MarketComments.module.scss';
 
 function MarketNewComment() {
+  const isLoggedIn = useAppSelector(state => state.polkamarkets.isLoggedIn);
+
+  const user = useAppSelector(state => state.polkamarkets.socialLoginInfo);
+  const isLoadingUser = useAppSelector(
+    state => state.polkamarkets.isLoading.login
+  );
+
+  const username = user?.name?.includes('#')
+    ? user?.name?.split('#')[0]
+    : user?.name?.split('@')[0];
+
+  const avatar = user?.profileImage;
+
   return (
     <form action="">
       <div className={styles.newComment}>
         <Avatar
-          src="https://drive.google.com/uc?id=184zxQaHisHzJnKO1L1FbelwRV4O5EFAT&export=download"
-          alt=""
+          src={avatar}
+          alt={username || 'Avatar'}
           $size="sm"
           className={styles.commentAvatar}
         />
@@ -25,11 +38,29 @@ function MarketNewComment() {
             rows={6}
             className={styles.newCommentBoxTextarea}
             placeholder="Write a comment..."
+            disabled={isLoadingUser || !isLoggedIn}
           />
           <div className={styles.newCommentBoxFooter}>
-            <Button type="submit" size="xs" color="primary">
-              Comment
-            </Button>
+            {!isLoadingUser && !isLoggedIn ? (
+              <ProfileSignin size="xs" color="primary">
+                <Icon
+                  name="LogIn"
+                  size="md"
+                  className={styles.newCommentBoxFooterSigninIcon}
+                />
+                Login
+              </ProfileSignin>
+            ) : null}
+            {isLoadingUser || (!isLoadingUser && isLoggedIn) ? (
+              <Button
+                type="submit"
+                size="xs"
+                color="primary"
+                disabled={isLoadingUser}
+              >
+                Comment
+              </Button>
+            ) : null}
           </div>
         </div>
       </div>
