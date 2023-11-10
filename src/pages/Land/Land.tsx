c10bf5070db6aa4dd3487d892772a395721411f4
Wellington Matheus <w.matheus@outlook.com>
2023-11-10 16:38:44 -0300

    feat(Land): remove mock data

--- src/pages/Land/Land.tsx
+++ src/pages/Land/Land.tsx
@@ -1,24 +1,40 @@
 import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classNames from 'classnames';
 import isEmpty from 'lodash/isEmpty';
 import uniqBy from 'lodash/uniqBy';
-import { useGetMarketsByIdsQuery } from 'services/Polkamarkets';
+import {
+  useGetLandBySlugQuery,
+  useGetMarketsByIdsQuery
+} from 'services/Polkamarkets';
 import { Container } from 'ui';
 
+import { AlertMini } from 'components';
+
 import TournamentsUpcomingMarkets from '../Tournaments/TournamentsUpcomingMarkets';
-import mock from './Land.mock';
 import LandHero from './LandHero';
 
 function Land() {
-  const { title, description, imageUrl, bannerUrl, tournaments } = mock;
+  const { slug } = useParams<{ slug: string }>();
+
+  const {
+    data: land,
+    isLoading: isLoadingLand,
+    isFetching: isFetchingLand
+  } = useGetLandBySlugQuery({ slug }, { skip: !slug });
+
+  const isLoadingGetLandBySlugQuery = isLoadingLand || isFetchingLand;
+  const isEmptyLand = !land || isEmpty(land);
 
   const marketsIds = useMemo(() => {
+    if (isLoadingGetLandBySlugQuery || isEmptyLand) return [];
+
     return uniqBy(
-      tournaments.map(tournament => tournament.markets || []).flat(),
+      land.tournaments.map(tournament => tournament.markets || []).flat(),
       'slug'
     ).map(market => market.id);
-  }, [tournaments]);
+  }, [isEmptyLand, isLoadingGetLandBySlugQuery, land]);
 
   const {
     data: markets,
@@ -27,7 +43,7 @@ function Land() {
   } = useGetMarketsByIdsQuery(
     {
       ids: marketsIds,
-      networkId: `${tournaments?.[0]?.networkId}`
+      networkId: `${land?.tournaments?.[0]?.networkId}`
     },
     {
       skip: isEmpty(marketsIds)
@@ -36,12 +52,26 @@ function Land() {
 
   const isLoadingGetMarketsByIdsQuery = isLoadingMarkets || isFetchingMarkets;
 
-  if (isLoadingGetMarketsByIdsQuery)
+  if (isLoadingGetLandBySlugQuery || isLoadingGetMarketsByIdsQuery)
     return (
       <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
         <span className="spinner--primary" />
       </div>
     );
+
+  if (isEmptyLand)
+    return (
+      <div className="padding-y-5 padding-x-4 width-full border-solid border-1 border-radius-small">
+        <AlertMini
+          style={{ border: 'none' }}
+          styles="outline"
+          variant="information"
+          description="No data available at the moment."
+        />
+      </div>
+    );
+
+  const { title, description, imageUrl, bannerUrl } = land;
 
   return (
     <Container className={classNames('max-width-screen-xl')}>
