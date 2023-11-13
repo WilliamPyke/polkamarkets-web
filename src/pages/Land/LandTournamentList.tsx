d5c9f8eba448aac6e7563e7c9fbc6484c45f4a0f
Wellington Matheus <w.matheus@outlook.com>
2023-11-13 07:38:32 -0300

    feat(LandTournamentList): create empty state

--- src/pages/Land/LandTournamentList.tsx
+++ src/pages/Land/LandTournamentList.tsx
@@ -2,6 +2,8 @@ import type { Land } from 'types/land';
 
 import Tournament from 'pages/Tournaments/Tournament';
 
+import { AlertMini } from 'components';
+
 import styles from './LandTournamentList.module.scss';
 
 type LandTournamentListProps = {
@@ -14,13 +16,24 @@ function LandTournamentList({ tournaments }: LandTournamentListProps) {
       <div className={styles.rootHeader}>
         <h2 className={styles.rootTitle}>Tournaments</h2>
       </div>
-      <ul className={styles.list}>
-        {tournaments.map(tournament => (
-          <li key={tournament.id}>
-            <Tournament tournament={tournament} />
-          </li>
-        ))}
-      </ul>
+      {!tournaments.length ? (
+        <div className="padding-y-5 padding-x-4 width-full border-solid border-1 border-radius-medium">
+          <AlertMini
+            style={{ border: 'none' }}
+            styles="outline"
+            variant="information"
+            description="No tournaments available at the moment."
+          />
+        </div>
+      ) : (
+        <ul className={styles.list}>
+          {tournaments.map(tournament => (
+            <li key={tournament.id}>
+              <Tournament tournament={tournament} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
