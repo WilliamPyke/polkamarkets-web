55445d402f3b8d94438e76f75b41ff7f13195781
Wellington Matheus <w.matheus@outlook.com>
2023-11-22 11:26:34 -0300

    feat(TradeContext): remove `transactionSuccessHash`

--- src/contexts/trade.tsx
+++ src/contexts/trade.tsx
@@ -4,15 +4,13 @@ import { Market, Outcome } from 'models/market';
 import { TradeType } from 'redux/ducks/trade';
 import { create } from 'zustand';
 
-import { Button, Feature, Toast, ToastNotification } from 'components';
+import { Button, Toast, ToastNotification } from 'components';
 
-import useNetwork from 'hooks/useNetwork';
 import useToastNotification from 'hooks/useToastNotification';
 
 export type TradeContextState = {
   type: TradeType;
   status: 'pending' | 'success' | 'error';
-  transactionSuccessHash?: string;
   trade: {
     market: Market['id'];
     outcome: Outcome['id'];
@@ -25,7 +23,6 @@ export type TradeContextState = {
 const initialState: TradeContextState = {
   type: 'buy',
   status: 'pending',
-  transactionSuccessHash: undefined,
   trade: {
     market: '',
     outcome: '',
@@ -47,11 +44,10 @@ export const TradeContext = createContext<TradeContextState>(
 );
 
 function TradeProvider({ children }) {
-  const { network } = useNetwork();
   const state = useTradeStore();
   const { show, close } = useToastNotification();
 
-  const { type, status, transactionSuccessHash } = state;
+  const { type, status } = state;
 
   useEffect(() => {
     if (status === 'success') {
@@ -69,19 +65,6 @@ function TradeProvider({ children }) {
             description="Your transaction is completed!"
           >
             <Toast.Actions>
-              <Feature name="regular">
-                {transactionSuccessHash ? (
-                  <a
-                    target="_blank"
-                    href={`${network.explorerURL}/tx/${transactionSuccessHash}`}
-                    rel="noreferrer"
-                  >
-                    <Button size="sm" color="success">
-                      View on Explorer
-                    </Button>
-                  </a>
-                ) : null}
-              </Feature>
               <Button size="sm" variant="ghost" onClick={() => close(type)}>
                 Dismiss
               </Button>
