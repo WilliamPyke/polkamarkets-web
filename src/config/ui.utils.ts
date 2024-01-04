37f2406e84ddada7f74dcc7d9c791803d537e0c1
Wellington Matheus <w.matheus@outlook.com>
2024-01-04 14:06:58 -0300

    feat(UIConfigUtils): create `parseNavbarItemsFromEnv`

--- src/config/ui.utils.ts
+++ src/config/ui.utils.ts
@@ -1,4 +1,5 @@
-/* eslint-disable import/prefer-default-export */
+import has from 'lodash/has';
+
 type Filter = {
   name: string;
   values: string[];
@@ -24,4 +25,27 @@ function parseFiltersFromEnv(variable: string | undefined): Filter[] {
   }
 }
 
-export { parseFiltersFromEnv };
+type NavbarItem = {
+  title: string;
+  href: string;
+};
+
+function parseNavbarItemsFromEnv(variable: string | undefined): NavbarItem[] {
+  if (!variable) {
+    return [];
+  }
+
+  try {
+    const parsedVariable = JSON.parse(variable);
+
+    const navbarItems = parsedVariable.filter(item =>
+      ['title', 'href'].every(key => has(item, key))
+    ) as NavbarItem[];
+
+    return navbarItems.map(({ title, href }) => ({ title, href }));
+  } catch (error) {
+    return [];
+  }
+}
+
+export { parseFiltersFromEnv, parseNavbarItemsFromEnv };
