6839651e11a3c6787619447bd0f59acc18efadd0
Wellington Matheus <w.matheus@outlook.com>
2024-01-27 13:54:03 -0300

    feat(UserTypes): add `marketId`, `outcomeId` and `networkId` to `UserOperation`

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,7 +2,10 @@ export type UserOperation = {
   imageUrl: string | null;
   marketSlug: string;
   marketTitle: string;
+  marketId: number;
   outcomeTitle: string;
+  outcomeId: number;
+  networkId: number;
   shares: number | null;
   status: 'success' | 'failed' | 'pending';
   action: 'buy' | 'sell';
