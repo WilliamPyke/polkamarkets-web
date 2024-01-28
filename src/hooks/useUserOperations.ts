90ced9c673a79894ef759771370ac20754623b8f
Wellington Matheus <w.matheus@outlook.com>
2024-01-28 08:18:30 -0300

    feat(useUserOperations): create `updateOperationStatus`

--- src/hooks/useUserOperations.ts
+++ src/hooks/useUserOperations.ts
@@ -1,8 +1,17 @@
-import { useGetUserOperationsByAddressQuery } from 'services/Polkamarkets';
+import { useCallback } from 'react';
 
+import map from 'lodash/map';
+import polkamarketsApi, {
+  useGetUserOperationsByAddressQuery
+} from 'services/Polkamarkets';
+import type { UserOperation } from 'types/user';
+
+import useAppDispatch from './useAppDispatch';
 import useAppSelector from './useAppSelector';
 
 function useUserOperations() {
+  const dispatch = useAppDispatch();
+
   const { login: isLoadingLogin } = useAppSelector(
     state => state.polkamarkets.isLoading
   );
@@ -16,7 +25,33 @@ function useUserOperations() {
       { skip: !isLoggedIn || isLoadingLogin }
     );
 
-  return { data, isLoading: isLoading || isFetching, refetch };
+  const updateOperationStatus = useCallback(
+    ({
+      userOperationHash,
+      status
+    }: Pick<UserOperation, 'userOperationHash' | 'status'>): void =>
+      dispatch(
+        polkamarketsApi.util.updateQueryData(
+          'getUserOperationsByAddress',
+          { address: userAddress },
+          operations => {
+            return map(operations, operation =>
+              operation.userOperationHash === userOperationHash
+                ? { ...operation, status }
+                : operation
+            );
+          }
+        )
+      ),
+    [dispatch, userAddress]
+  );
+
+  return {
+    data,
+    isLoading: isLoading || isFetching,
+    refetch,
+    updateOperationStatus
+  };
 }
 
 export default useUserOperations;
