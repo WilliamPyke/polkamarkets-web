9822efd544efb5ec98fa18cafa3224d924eb40ab
Victor <soujvnunes@gmail.com>
2024-01-29 11:21:07 -0300

    feat(useReloadMarketPrices): compose reloader

---
+++ src/hooks/useReloadMarketPrices.ts
@@ -0,0 +1,39 @@
+import { useCallback } from 'react';
+
+import { Market } from 'models/market';
+import PolkamarketsService from 'services/PolkamarketsService';
+
+import useAppDispatch from 'hooks/useAppDispatch';
+import useNetwork from 'hooks/useNetwork';
+
+export default function useReloadMarketPrices({
+  id: marketId
+}: Pick<Market, 'id'>) {
+  const dispatch = useAppDispatch();
+  const { networkConfig } = useNetwork();
+
+  return useCallback(async () => {
+    const { changeMarketOutcomeData, changeMarketData } = await import(
+      'redux/ducks/markets'
+    );
+    const { changeOutcomeData, changeData } = await import(
+      'redux/ducks/market'
+    );
+    const marketData = await new PolkamarketsService(
+      networkConfig
+    ).getMarketData(marketId);
+    const liquidityData = { data: { liquidity: marketData.liquidity } };
+
+    (marketData.outcomes as Array<Record<'price' | 'shares', number>>).forEach(
+      (data, id) => {
+        const payload = { marketId, id, data };
+
+        dispatch(changeMarketOutcomeData(payload));
+        dispatch(changeOutcomeData(payload));
+      }
+    );
+
+    dispatch(changeMarketData({ marketId, ...liquidityData }));
+    dispatch(changeData(liquidityData));
+  }, [dispatch, marketId, networkConfig]);
+}
