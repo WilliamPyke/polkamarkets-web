0d6b76be96dbf4e2b05e80bbf91bb2f804402ea6
Victor <soujvnunes@gmail.com>
2024-02-09 13:37:54 -0300

    feat(TournamentTopUsers): add ranking tab

--- src/pages/Tournament/TournamentTopUsers.tsx
+++ src/pages/Tournament/TournamentTopUsers.tsx
@@ -1,9 +1,17 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import cn from 'classnames';
+import classNames from 'classnames';
+import capitalize from 'lodash/capitalize';
+import every from 'lodash/every';
+import isEmpty from 'lodash/isEmpty';
+import isNull from 'lodash/isNull';
+import Divider from 'ui/Divider';
 
-import LeaderboardStats from '../Leaderboard/LeaderboardStats';
+import AlertMini from 'components/Alert/AlertMini';
+import Icon from 'components/Icon';
+import TableMini from 'components/new/TableMini/TableMini';
+
 import type { PrepareLeaderboardTableRowsArgs } from '../Leaderboard/prepare';
 import styles from './TournamentTopUsers.module.scss';
 import { TournamentTopUsersColumn } from './TournamentTopUsers.types';
@@ -37,28 +45,98 @@ type TournamentTopUsersProps = {
 function TournamentTopUsers({ rows, isLoading }: TournamentTopUsersProps) {
   const location = useLocation();
 
+  const [tab, setTab] = useState('ranking');
+
   const row = useMemo(() => prepareTournamentTopUsersRow({ rows }), [rows]);
+  const state = useMemo(() => {
+    if (isLoading) return 'loading';
+    if (
+      isEmpty(row) ||
+      every(
+        Object.values(row).map(v => v.value),
+        isNull
+      )
+    )
+      return 'error';
+    return 'success';
+  }, [isLoading, row]);
+
+  const handleTab = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setTab(event.currentTarget.value);
+    },
+    []
+  );
 
   return (
-    <LeaderboardStats
-      title="Top Users"
-      columns={columns}
-      row={row}
-      isLoading={isLoading}
-      action={
-        <Link
-          to={`${location.pathname}/leaderboard`}
-          className={cn(
-            'pm-c-button--sm',
-            'pm-c-button--fullwidth',
-            styles.rootActionsButton
-          )}
-        >
-          View Leaderboard
-        </Link>
+    <div
+      className={classNames(
+        'border-radius-medium border-solid border-1',
+        styles.root
+      )}
+    >
+      <div className={classNames('pm-c-leaderboard-stats bg-3')}>
+        <div className={styles.tabs}>
+          {['ranking', 'rewards'].map(_tab => (
+            <button
+              role="tab"
+              type="button"
+              value={_tab}
+              key={_tab}
+              aria-selected={tab === _tab}
+              className={styles.tabsItem}
+              onClick={handleTab}
+            >
+              {capitalize(_tab)}
+            </button>
+          ))}
+        </div>
+        {
+          {
+            loading: (
+              <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+                <span className="spinner--primary" />
+              </div>
+            ),
+            error: (
+              <AlertMini
+                style={{ border: 'none' }}
+                styles="outline"
+                variant="information"
+                description="No data to show."
+              />
+            ),
+            success: (
+              // @ts-ignore
+              <TableMini
+                columns={tab === 'ranking' ? columns : columns}
+                row={tab === 'ranking' ? row : row}
+              />
+            )
+          }[state]
+        }
+      </div>
+      <Divider />
+      {
+        {
+          ranking: (
+            <Link
+              to={`${location.pathname}/leaderboard`}
+              className={styles.action}
+            >
+              View Leaderboard
+              <Icon
+                name="Arrow"
+                size="md"
+                dir="right"
+                className={styles.actionIcon}
+              />
+            </Link>
+          ),
+          rewards: 'ranking'
+        }[tab]
       }
-      className={{ root: styles.root }}
-    />
+    </div>
   );
 }
 
