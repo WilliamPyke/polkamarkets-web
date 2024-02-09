9705da160e5d1839dc20d03dc7efc9d58294bb55
Victor <soujvnunes@gmail.com>
2024-02-09 16:44:47 -0300

    feat(LeaderboardRewards): implement list as a chid

--- src/pages/Leaderboard/LeaderboardRewards.tsx
+++ src/pages/Leaderboard/LeaderboardRewards.tsx
@@ -1,33 +1,10 @@
-import { Text } from 'components';
-
-import classes from './LeaderboardRewards.module.scss';
-
-type LeaderboardRewardsProps = {
-  rewards: Array<Record<'title' | 'description', string>>;
-};
-
 export default function LeaderboardRewards({
-  rewards
-}: LeaderboardRewardsProps) {
+  children
+}: React.PropsWithChildren<{}>) {
   return (
     <div className="pm-c-leaderboard-stats bg-3 border-radius-medium border-solid border-1">
       <h3 className="body semibold text-1">Rewards</h3>
-      <ul>
-        {rewards.map(reward => (
-          <li key={reward.title} className={classes.rewardsReward}>
-            <Text
-              scale="caption"
-              fontWeight="medium"
-              className={classes.rewardsRewardPrimary}
-            >
-              {reward.title}
-            </Text>
-            <Text scale="caption" color="lighter-gray">
-              {reward.description}
-            </Text>
-          </li>
-        ))}
-      </ul>
+      {children}
     </div>
   );
 }
