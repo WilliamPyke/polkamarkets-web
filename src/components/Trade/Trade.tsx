727604a12bb9ad480cb76c4dbb643e639f4cd949
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-02-29 14:12:27 +0000

    fix: eslint issues

--- src/components/Trade/Trade.tsx
+++ src/components/Trade/Trade.tsx
@@ -2,7 +2,6 @@ import { CSSProperties, useCallback, useEffect, useMemo } from 'react';
 
 import cn from 'classnames';
 import { features } from 'config';
-
 import isEmpty from 'lodash/isEmpty';
 import isUndefined from 'lodash/isUndefined';
 import { changeTradeType, selectOutcome } from 'redux/ducks/trade';
@@ -23,7 +22,6 @@ import TradeDetails from './TradeDetails';
 import TradeMarketShares from './TradeMarketShares';
 import TradePredictions from './TradePredictions';
 import TradeTypeSelector from './TradeTypeSelector';
-
 
 type TradeProps = {
   view?: View;
@@ -185,7 +183,9 @@ function Trade({ view = 'default', onTradeFinished }: TradeProps) {
               </p>
             </div>
           ) : null}
-          {!isEmpty(outcomesWithShares) && !needsSellSharesOfOtherOutcomes ? <TradeTypeSelector /> : null}
+          {!isEmpty(outcomesWithShares) && !needsSellSharesOfOtherOutcomes ? (
+            <TradeTypeSelector />
+          ) : null}
           <TradeMarketShares />
           {!needsSellSharesOfOtherOutcomes ? (
             <TradeFormInput
